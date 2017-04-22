b63e019e9445407252f3353e8cbaf64222932fd5
cesar avina <cesaravina@gmail.com>
2017-04-22 16:42:36 -0700

    dependencies

---
+++ customer.js
@@ -0,0 +1,13 @@
+// Dependencies
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'Bamazon'
+});
+
